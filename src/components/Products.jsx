c71426248d61b28966134e628d6eb2c5697e4ed1
MoysisPap <144357526+MoysisPap@users.noreply.github.com>
2024-09-16 14:08:41 +0200

    paths

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,34 +1,76 @@
 // src/components/Products.jsx
 import { useContext } from 'react';
 import { CartContext } from './CartContext.jsx';
-import product1 from '/assets/product1.png';
-import product2 from '/assets/product2.png';
-import product3 from '/assets/product3.png';
-import product4 from '/assets/product4.png';
-import product5 from '/assets/product5.png';
-import product6 from '/assets/product6.png';
-import product7 from '/assets/product7.png';
-import product8 from '/assets/product8.png';
-import product9 from '/assets/product9.png';
-import product10 from '/assets/product10.png';
-import product11 from '/assets/product11.png';
-import product12 from '/assets/product12.png';
 
 const products = [
-  { id: 1, name: 'Official 1st Home Jersey', price: 75, image: product1 },
-  { id: 2, name: 'Official 2nd Home Jersey', price: 75, image: product2 },
-  { id: 3, name: 'Official 1st Away Jersey', price: 75, image: product3 },
-  { id: 4, name: 'Official 2nd Away Jersey', price: 75, image: product4 },
-  { id: 5, name: 'PAOK FC Anthem Jacket', price: 70, image: product5 },
-  { id: 6, name: 'PAOK FC Black Hoodie', price: 60, image: product6 },
-  { id: 7, name: 'PAOK FC Travel Jacket', price: 120, image: product7 },
-  { id: 8, name: 'T-shirt Champions 2024', price: 20, image: product8 },
-  { id: 9, name: 'PAOK FC Black Pants', price: 40, image: product9 },
-  { id: 10, name: 'PAOK FC Grey Beanie', price: 20, image: product10 },
-  { id: 11, name: 'PAOK FC Black Cap 4', price: 30, image: product11 },
-  { id: 12, name: 'Memorial Cup', price: 25, image: product12 },
+  {
+    id: 1,
+    name: 'Official 1st Home Jersey',
+    price: 75,
+    image: '/assets/product1.png',
+  },
+  {
+    id: 2,
+    name: 'Official 2nd Home Jersey',
+    price: 75,
+    image: '/assets/product1.png',
+  },
+  {
+    id: 3,
+    name: 'Official 1st Away Jersey',
+    price: 75,
+    image: '/assets/product1.png',
+  },
+  {
+    id: 4,
+    name: 'Official 2nd Away Jersey',
+    price: 75,
+    image: '/assets/product1.png',
+  },
+  {
+    id: 5,
+    name: 'PAOK FC Anthem Jacket',
+    price: 70,
+    image: '/assets/product1.png',
+  },
+  {
+    id: 6,
+    name: 'PAOK FC Black Hoodie',
+    price: 60,
+    image: '/assets/product1.png',
+  },
+  {
+    id: 7,
+    name: 'PAOK FC Travel Jacket',
+    price: 120,
+    image: '/assets/product1.png',
+  },
+  {
+    id: 8,
+    name: 'T-shirt Champions 2024',
+    price: 20,
+    image: '/assets/product1.png',
+  },
+  {
+    id: 9,
+    name: 'PAOK FC Black Pants',
+    price: 40,
+    image: '/assets/product1.png',
+  },
+  {
+    id: 10,
+    name: 'PAOK FC Grey Beanie',
+    price: 20,
+    image: '/assets/product1.png',
+  },
+  {
+    id: 11,
+    name: 'PAOK FC Black Cap 4',
+    price: 30,
+    image: '/assets/product1.png',
+  },
+  { id: 12, name: 'Memorial Cup', price: 25, image: '/assets/product1.png' },
 ];
-
 
 const Products = () => {
   const { addToCart } = useContext(CartContext);
